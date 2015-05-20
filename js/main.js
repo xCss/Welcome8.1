a3da039f132b4ea3f998177a7623ed6978c6a91c
ioliu <yy.liu@foxmail.com>
2015-05-20 18:24:00 +0800

    Modify the animation sequence

--- js/main.js
+++ js/main.js
@@ -47,11 +47,12 @@ window.onload = function(){
         setTimeout(function(){
             alpha(text,TIPS[i],false,2000,function(){
                 alpha(text,TIPS[++i],true,2000);
+                setInterval(function(){
+                    rgb = color(rgb);
+                    $('.main').style.backgroundColor = "rgb("+rgb.join(',')+")";
+                },20);
             });
-            setInterval(function(){
-                rgb = color(rgb);
-                $('.main').style.backgroundColor = "rgb("+rgb.join(',')+")";
-            },20);
+
         },1000);
     });
 
